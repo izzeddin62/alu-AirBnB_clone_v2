0a18a76072803506549ab77b55cf4e8cb4234569
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-03-20 15:56:10 -0700

    changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """starts a Flask web application that listens on 0.0.0.0, port 5000,
-and has a single route that displays "Hello HBNB!" when accessed.
+and has 2 routes:
+- "/" displays "Hello HBNB!"
+- "/hbnb" displays "HBNB"
 """
 from flask import Flask
 
@@ -10,8 +12,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return("HBNB")
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
