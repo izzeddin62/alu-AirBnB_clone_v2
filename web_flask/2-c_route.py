63fb2f7767e9d8b046cfd17e789421a922797e38
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-03-22 09:03:56 -0700

    changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,5 +20,9 @@ def hbnb():
     return("HBNB")
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
